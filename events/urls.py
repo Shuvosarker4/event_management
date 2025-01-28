86051b6bf6e30c91d581cb38519bc08b69e2fb85
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-01-28 12:00:52 +0600

    event search filter added

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from events.views import home_page,dashboard,details,create_event
 
 urlpatterns = [
     path('home-page/', home_page,name="home"),
-    path('dashboard/', dashboard),
+    path('dashboard/', dashboard,name="dashboard"),
     path('details/<int:event_id>/', details,name='event_details'),
-    path('create-event/', create_event),
+    path('create-event/', create_event,name="create-event"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from events.models import Event
+from events.models import Event,Participant
 from django.db.models import Q
 # Create your views here.
 def home_page(request):
@@ -7,22 +7,25 @@ def home_page(request):
     if query:
         all_events = Event.objects.filter(
             Q(name__icontains=query) |
-            Q(category__name__icontains=query) |
             Q(location__icontains=query)
         )
         if not all_events.exists():
             return render(request, 'home_page/home_page.html', {"message": "Event not found"})
     else:
         all_events = Event.objects.all()
-    print(all_events)
     context = {"events":all_events,"query": query}
     return render(request,'home_page/home_page.html',context)
 
 def dashboard(request):
+    type = request.GET.get('total-event')
+    if type == 'total-event':
+        all_events = Event.objects.all()
     all_events = Event.objects.all()
     total_event = all_events.count()
-    print()
-    context = {"total_event":total_event}
+    total_participant = Participant.objects.count()
+    context = {"total_event":total_event,
+               "total_participant":total_participant,
+               "all_events":all_events}
     return render(request,'dashboard/dashboard.html',context)
 
 def details(request,event_id):
