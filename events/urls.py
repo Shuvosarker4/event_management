07f74a83346b26c55112c40959668d53f2412193
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-01-28 09:50:42 +0600

    search option implement with details page

--- events/urls.py
+++ events/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from events.views import home_page,dashboard,details,create_event
 
 urlpatterns = [
-    path('home-page/', home_page),
+    path('home-page/', home_page,name="home"),
     path('dashboard/', dashboard),
-    path('details/', details),
+    path('details/<int:event_id>/', details,name='event_details'),
     path('create-event/', create_event),
 ]
--- events/views.py
+++ events/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from events.models import Event
+from django.db.models import Q
 # Create your views here.
 def home_page(request):
-    all_events = Event.objects.all()
-    context = {"events":all_events}
+    query = request.GET.get('q')
+    if query:
+        all_events = Event.objects.filter(
+            Q(name__icontains=query) |
+            Q(category__name__icontains=query) |
+            Q(location__icontains=query)
+        )
+        if not all_events.exists():
+            return render(request, 'home_page/home_page.html', {"message": "Event not found"})
+    else:
+        all_events = Event.objects.all()
+    print(all_events)
+    context = {"events":all_events,"query": query}
     return render(request,'home_page/home_page.html',context)
 
 def dashboard(request):
     return render(request,'dashboard/dashboard.html')
 
-def details(request):
-    return render(request,'details/details.html')
+def details(request,event_id):
+    event = Event.objects.get(id=event_id)
+    print(event.name)
+    return render(request,'details/details.html',{"event":event})
 
 
 def create_event(request):
