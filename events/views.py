6501c484703c16a7e779c39348dec75f966d28f1
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-02-04 21:46:12 +0600

    requirements 2

--- events/views.py
+++ events/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render,redirect,HttpResponse
 from events.models import Event,RSVP
 from events.forms import EventModelForm,CategoryModelForm,CustomRegisterForm,LoginForm,AssignRoleForm,CreateGroupForm
 from django.db.models import Q
-from django.conf import settings
-from django.core.mail import send_mail
 from django.contrib import messages
 from django.utils import timezone
 from django.contrib.auth import login,logout
@@ -192,8 +190,7 @@ def assign_role(request,user_id):
             role = form.cleaned_data.get('role')
             user.groups.clear()
             user.groups.add(role)
-            messages.success(request, f"User {
-                             user.username} has been assigned to the {role.name} role")
+            messages.success(request, f"User {user.username} has been assigned to the {role.name} role")
             return redirect('admin-dashboard')
 
     return render(request, 'admin/assign_role.html', {"form": form})
@@ -206,8 +203,7 @@ def create_group(request):
 
         if form.is_valid():
             group = form.save()
-            messages.success(request, f"Group {
-                             group.name} has been created successfully")
+            messages.success(request, f"Group {group.name} has been created successfully")
             return redirect('create-group')
 
     return render(request, 'admin/create_group.html', {'form': form})
